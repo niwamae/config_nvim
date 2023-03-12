e203cf591d26e35c1d22f177a6f9b68809ec6dee
Nobuyuki Iwamae <n.iw@me.com>
2023-03-12 16:56:33 +0900

    - Add panda theme - Unset relative number

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,6 +1,7 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
+  "markvincze/panda-vim",
   -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",
